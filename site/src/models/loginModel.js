e8abe57d49b0e6ccdd11d45aed1b4048f9eadeb8
linyamendonca <linya.mendonca@sptech.school>
2025-04-06 11:05:00 -0300

    ajuste src/model nomenclaturas

--- site/src/models/loginModel.js
+++ site/src/models/loginModel.js
@@ -3,7 +3,7 @@ var database = require("../database/config")
 function autenticar(conselho, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", conselho, senha)
     var instrucaoSql = `
-        SELECT idUsuario, nomeCompleto, dataNascimento, caroExercido, numConselho, cidadeResidente FROM cadastro WHERE email = '${conselho}' AND senha = MD5('${senha}');
+        SELECT idUsuario, nomeCompleto, dataNascimento, cargoExercido, numConselho, cidadeResidente FROM cadastro WHERE email = '${conselho}' AND senha = MD5('${senha}');
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
