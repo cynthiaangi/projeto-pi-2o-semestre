695e70151ebdfa7a8c87e46003d94184d05ff038
cynthiaangi <cynthia.angi@sptech.school>
2025-04-27 22:21:27 -0300

    todas as doencas funcionando

--- site/public/js/dashboard.js
+++ site/public/js/dashboard.js
@@ -1,7 +1,20 @@
+// graficos Coqueluche
 var ctx = myChartCanvas
 var cty = myChartCanvas2
 var ctz = myChartCanvas3
 var ctw = myChartCanvas4
+
+// graficos Meningite
+var cta = myChartCanvas5
+var ctb = myChartCanvas6
+var ctc = myChartCanvas7
+var ctd = myChartCanvas8
+
+// graficos Poliomielite
+var cti = myChartCanvas9
+var ctj = myChartCanvas10
+var ctk = myChartCanvas11
+var ctl = myChartCanvas12
 
 const doencas = ['Coqueluche', 'Meningite', 'Poliomielite'];
 
@@ -70,7 +83,202 @@ let myChart2 = new Chart(cty, {
         plugins: {
             title: {
                 display: true,
-                text: 'Meta vacinal para coqueluche',
+                text: 'Meta vacinal para Coqueluche',
+                color: '#2e2e2e',
+                font: {
+                    size: 24
+                }
+            },
+            legend: {
+                labels: {
+                    color: '#2e2e2e',
+                    font: {
+                        size: 16
+                    }
+                }
+            },
+            tooltip: { enabled: true }
+        }
+    },
+    plugins: [{
+        id: 'marcadorMeta',
+        afterDatasetsDraw(chart) {
+            const { ctx, chartArea } = chart;
+            if (!chartArea) return; // garante que o gráfico será desenhado apenas após a caixa dele estiver pronta
+
+            const posicao = 85; // ajuste na inclinação e posição da marca no gráfico
+            const angle = Math.PI * (1 - posicao / 100); // Invertido (vai de 0% à esquerda até 100% à direita)
+
+            const centerX = (chartArea.left + chartArea.right) / 2;
+            const centerY = chartArea.bottom;
+            const radius = (chartArea.right - chartArea.left) / 2 * 0.8;
+
+            const x = centerX + radius * Math.cos(angle - 0.5);
+            const y = centerY - radius * Math.cos(angle + 0.5);
+
+            const marcadorComprimento = 30; // comprimento da marca da meta
+            const inclinacao = 0.5; // controla a inclinação da marca da meta
+
+            ctx.save();
+            ctx.beginPath();
+            ctx.moveTo(
+                x - marcadorComprimento * Math.cos(angle - inclinacao),
+                y + marcadorComprimento * Math.sin(angle - inclinacao)
+            ); // de onde começa
+            ctx.lineTo(
+                x + marcadorComprimento * Math.cos(angle + inclinacao),
+                y - marcadorComprimento * Math.sin(angle + inclinacao)
+            ); // onde termina
+            ctx.strokeStyle = 'limegreen';
+            ctx.lineWidth = 3;
+            ctx.stroke();
+            ctx.restore();
+
+            ctx.save();
+            ctx.fillStyle = '#444';
+            ctx.font = 'bold 14px Arial';
+
+            // Posição meta
+            const posicaoMeta = Math.PI;
+            const xMeta = x + marcadorComprimento * Math.cos(angle + inclinacao);
+            const yMeta = y - marcadorComprimento * Math.sin(angle + inclinacao);
+            ctx.fillText('95', xMeta - 5, yMeta - 10);
+
+            // Posição do 0 (lado esquerdo)
+            const posicao0 = Math.PI;
+            const x0 = centerX + (radius + 30) * Math.cos(posicao0);
+            const y0 = (centerY - 20) - (radius + 15) * Math.sin(posicao0);
+            ctx.fillText('0', x0, y0); // ajustezinho fino
+
+            // Posição do 100 (lado direito)
+            const posicao100 = 0;
+            const x100 = centerX + (radius + 20) * Math.cos(posicao100);
+            const y100 = (centerY - 25) - (radius + 15) * Math.sin(posicao100);
+            ctx.fillText('100', x100 - 15, y100 + 5); // ajustezinho fino
+
+            ctx.restore();
+        }
+    }]
+
+
+});
+let myChart6 = new Chart(ctb, {
+    type: 'doughnut',
+    data: {
+        labels: ['Onde estamos %'],
+        datasets: [{
+            data: [75, 100 - 75],
+            backgroundColor: ['#0A4D68', '#E0E0E0'],
+            borderWidth: 0,
+            circumference: 180,
+            rotation: 270,
+            cutout: '70%',
+        }]
+    },
+    options: {
+        responsive: true,
+        plugins: {
+            title: {
+                display: true,
+                text: 'Meta vacinal para Meningite',
+                color: '#2e2e2e',
+                font: {
+                    size: 24
+                }
+            },
+            legend: {
+                labels: {
+                    color: '#2e2e2e',
+                    font: {
+                        size: 16
+                    }
+                }
+            },
+            tooltip: { enabled: true }
+        }
+    },
+    plugins: [{
+        id: 'marcadorMeta',
+        afterDatasetsDraw(chart) {
+            const { ctx, chartArea } = chart;
+            if (!chartArea) return; // garante que o gráfico será desenhado apenas após a caixa dele estiver pronta
+
+            const posicao = 85; // ajuste na inclinação e posição da marca no gráfico
+            const angle = Math.PI * (1 - posicao / 100); // Invertido (vai de 0% à esquerda até 100% à direita)
+
+            const centerX = (chartArea.left + chartArea.right) / 2;
+            const centerY = chartArea.bottom;
+            const radius = (chartArea.right - chartArea.left) / 2 * 0.8;
+
+            const x = centerX + radius * Math.cos(angle - 0.5);
+            const y = centerY - radius * Math.cos(angle + 0.5);
+
+            const marcadorComprimento = 30; // comprimento da marca da meta
+            const inclinacao = 0.5; // controla a inclinação da marca da meta
+
+            ctx.save();
+            ctx.beginPath();
+            ctx.moveTo(
+                x - marcadorComprimento * Math.cos(angle - inclinacao),
+                y + marcadorComprimento * Math.sin(angle - inclinacao)
+            ); // de onde começa
+            ctx.lineTo(
+                x + marcadorComprimento * Math.cos(angle + inclinacao),
+                y - marcadorComprimento * Math.sin(angle + inclinacao)
+            ); // onde termina
+            ctx.strokeStyle = 'limegreen';
+            ctx.lineWidth = 3;
+            ctx.stroke();
+            ctx.restore();
+
+            ctx.save();
+            ctx.fillStyle = '#444';
+            ctx.font = 'bold 14px Arial';
+
+            // Posição meta
+            const posicaoMeta = Math.PI;
+            const xMeta = x + marcadorComprimento * Math.cos(angle + inclinacao);
+            const yMeta = y - marcadorComprimento * Math.sin(angle + inclinacao);
+            ctx.fillText('95', xMeta - 5, yMeta - 10);
+
+            // Posição do 0 (lado esquerdo)
+            const posicao0 = Math.PI;
+            const x0 = centerX + (radius + 30) * Math.cos(posicao0);
+            const y0 = (centerY - 20) - (radius + 15) * Math.sin(posicao0);
+            ctx.fillText('0', x0, y0); // ajustezinho fino
+
+            // Posição do 100 (lado direito)
+            const posicao100 = 0;
+            const x100 = centerX + (radius + 20) * Math.cos(posicao100);
+            const y100 = (centerY - 25) - (radius + 15) * Math.sin(posicao100);
+            ctx.fillText('100', x100 - 15, y100 + 5); // ajustezinho fino
+
+            ctx.restore();
+        }
+    }]
+
+
+});
+
+let myChart10 = new Chart(ctj, {
+    type: 'doughnut',
+    data: {
+        labels: ['Onde estamos %'],
+        datasets: [{
+            data: [75, 100 - 75],
+            backgroundColor: ['#0A4D68', '#E0E0E0'],
+            borderWidth: 0,
+            circumference: 180,
+            rotation: 270,
+            cutout: '70%',
+        }]
+    },
+    options: {
+        responsive: true,
+        plugins: {
+            title: {
+                display: true,
+                text: 'Meta vacinal para Poliomielite',
                 color: '#2e2e2e',
                 font: {
                     size: 24
@@ -163,17 +371,67 @@ let myChart = new Chart(ctx,
                 borderColor: '#0A4D68',
                 backgroundColor: '#0A4D68',
                 tension: 0.1
+            }]
+        },
+        options: {
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'Quantidade de casos de Coqueluche por ano',
+                    color: '#2e2e2e',
+                    font: {
+                        size: 24
+                    }
+                },
+                legend: {
+                    labels: {
+                        color: '#2e2e2e',
+                        font: {
+                            size: 16
+                        }
+                    }
+                }
             },
-            {
-                label: 'Poliomielite',
-                data: [],
-                borderColor: "#8a8a8a",
-                backgroundColor: "#8a8a8a",
-                tension: 0.1
-            },
-            {
+            scales: {
+                x: {
+                    ticks: {
+                        color: '#2e2e2e',
+                    },
+                    title: {
+                        display: true,
+                        text: 'anos',
+                        color: '#2e2e2e',
+                        font: {
+                            size: 16
+                        }
+                    }
+                },
+                y: {
+                    beginAtZero: true,
+                    ticks: {
+                        color: '#2e2e2e',
+                    },
+                    title: {
+                        display: true,
+                        text: 'qtde. casos',
+                        color: '#2e2e2e',
+                        font: {
+                            size: 16
+                        }
+                    }
+                }
+            }
+        }
+    });
+
+let myChart5 = new Chart(cta,
+    {
+        type: 'line',
+        data: {
+            labels: ['2018', '2019', '2020', '2021', '2022'],
+            datasets: [{
                 label: 'Meningite',
-                data: [],
+                data: ['100', '400', '342', '298', '792'],
                 borderColor: "#99ccff",
                 backgroundColor: "#99ccff",
                 tension: 0.1
@@ -183,7 +441,71 @@ let myChart = new Chart(ctx,
             plugins: {
                 title: {
                     display: true,
-                    text: 'Quantidade de casos por ano',
+                    text: 'Quantidade de casos de Meningite por ano',
+                    color: '#2e2e2e',
+                    font: {
+                        size: 24
+                    }
+                },
+                legend: {
+                    labels: {
+                        color: '#2e2e2e',
+                        font: {
+                            size: 16
+                        }
+                    }
+                }
+            },
+            scales: {
+                x: {
+                    ticks: {
+                        color: '#2e2e2e',
+                    },
+                    title: {
+                        display: true,
+                        text: 'anos',
+                        color: '#2e2e2e',
+                        font: {
+                            size: 16
+                        }
+                    }
+                },
+                y: {
+                    beginAtZero: true,
+                    ticks: {
+                        color: '#2e2e2e',
+                    },
+                    title: {
+                        display: true,
+                        text: 'qtde. casos',
+                        color: '#2e2e2e',
+                        font: {
+                            size: 16
+                        }
+                    }
+                }
+            }
+        }
+    });
+
+let myChart9 = new Chart(cti,
+    {
+        type: 'line',
+        data: {
+            labels: ['2018', '2019', '2020', '2021', '2022'],
+            datasets: [{
+                label: 'Poliomielite',
+                data: ['100', '400', '342', '298', '792'],
+                borderColor: "#8a8a8a",
+                backgroundColor: "#8a8a8a",
+                tension: 0.1
+            }]
+        },
+        options: {
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'Quantidade de casos de Poliomielite por ano',
                     color: '#2e2e2e',
                     font: {
                         size: 24
@@ -268,6 +590,81 @@ let myChart3 = new Chart(ctz, {
 
 });
 
+let myChart7 = new Chart(ctc, {
+    type: 'doughnut',
+    data: {
+        labels: ['Acima da meta', 'Abaixo sem risco', 'Com risco epidemiológico'],
+        datasets: [{
+            data: ['45', '35', '20'],
+            backgroundColor: ['#0A4D68', '#EBCF1C', '#EB3B30'],
+            borderWidth: 0,
+            circumference: 360,
+            rotation: 270,
+            cutout: '45%',
+        }]
+    },
+    options: {
+        responsive: true,
+        plugins: {
+            title: {
+                display: true,
+                text: 'Situação da cobertura vacinal no estado (%)',
+                color: '#2e2e2e',
+                font: {
+                    size: 24
+                }
+            },
+            legend: {
+                labels: {
+                    color: '#2e2e2e',
+                    font: {
+                        size: 10
+                    }
+                }
+            },
+            tooltip: { enabled: true }
+        }
+    }
+
+});
+let myChart11 = new Chart(ctk, {
+    type: 'doughnut',
+    data: {
+        labels: ['Acima da meta', 'Abaixo sem risco', 'Com risco epidemiológico'],
+        datasets: [{
+            data: ['34', '49', '17'],
+            backgroundColor: ['#0A4D68', '#EBCF1C', '#EB3B30'],
+            borderWidth: 0,
+            circumference: 360,
+            rotation: 270,
+            cutout: '45%',
+        }]
+    },
+    options: {
+        responsive: true,
+        plugins: {
+            title: {
+                display: true,
+                text: 'Situação da cobertura vacinal no estado (%)',
+                color: '#2e2e2e',
+                font: {
+                    size: 24
+                }
+            },
+            legend: {
+                labels: {
+                    color: '#2e2e2e',
+                    font: {
+                        size: 10
+                    }
+                }
+            },
+            tooltip: { enabled: true }
+        }
+    }
+
+});
+
 
 // Adicionando gráfico criado em div na tela
 let myChart4 = new Chart(ctw, {
@@ -277,6 +674,148 @@ let myChart4 = new Chart(ctw, {
         datasets: [{
             label: 'Vacinados',
             data: [15, 85, 5, 80, 20],
+            borderWidth: 1,
+            borderColor: '#0A4D68',
+            backgroundColor: '#0A4D68'
+        },
+        {
+            label: 'Não vacinados',
+            data: [15, 85, 5, 80, 20],
+            borderWidth: 1,
+            borderColor: '#99ccff',
+            backgroundColor: '#99ccff'
+        }
+        ]
+    },
+    options: {
+        plugins: {
+            title: {
+                display: true,
+                text: 'Situação vacinal ao longo dos anos',
+                color: 'rgb(0,0,0)',
+                font: {
+                    size: 24
+                }
+            },
+            legend: {
+                labels: {
+                    color: 'rgb(0, 0, 0)',
+                    font: {
+                        size: 16
+                    }
+                }
+            }
+        },
+        scales: {
+            x: {
+                ticks: {
+                    color: 'rgb(9, 9, 9)',
+                },
+                title: {
+                    display: true,
+                    text: 'anos',
+                    color: 'rgb(9, 9, 9)',
+                    font: {
+                        size: 16
+                    }
+                }
+            },
+            y: {
+                beginAtZero: true,
+                ticks: {
+                    color: 'rgb(9, 9, 9)',
+                },
+                title: {
+                    display: true,
+                    text: 'qtde. de pessoas (x1000)',
+                    color: 'rgb(9, 9, 9)',
+                    font: {
+                        size: 16
+                    }
+                }
+            }
+        }
+    }
+});
+
+let myChart8 = new Chart(ctd, {
+    type: 'bar',
+    data: {
+        labels: ['2018', '2019', '2020', '2021', '2022'],
+        datasets: [{
+            label: 'Vacinados',
+            data: [25, 15, 10, 8, 24],
+            borderWidth: 1,
+            borderColor: '#0A4D68',
+            backgroundColor: '#0A4D68'
+        },
+        {
+            label: 'Não vacinados',
+            data: [15, 85, 5, 80, 20],
+            borderWidth: 1,
+            borderColor: '#99ccff',
+            backgroundColor: '#99ccff'
+        }
+        ]
+    },
+    options: {
+        plugins: {
+            title: {
+                display: true,
+                text: 'Situação vacinal ao longo dos anos',
+                color: 'rgb(0,0,0)',
+                font: {
+                    size: 24
+                }
+            },
+            legend: {
+                labels: {
+                    color: 'rgb(0, 0, 0)',
+                    font: {
+                        size: 16
+                    }
+                }
+            }
+        },
+        scales: {
+            x: {
+                ticks: {
+                    color: 'rgb(9, 9, 9)',
+                },
+                title: {
+                    display: true,
+                    text: 'anos',
+                    color: 'rgb(9, 9, 9)',
+                    font: {
+                        size: 16
+                    }
+                }
+            },
+            y: {
+                beginAtZero: true,
+                ticks: {
+                    color: 'rgb(9, 9, 9)',
+                },
+                title: {
+                    display: true,
+                    text: 'qtde. de pessoas (x1000)',
+                    color: 'rgb(9, 9, 9)',
+                    font: {
+                        size: 16
+                    }
+                }
+            }
+        }
+    }
+});
+
+let myChart12 = new Chart(ctl, {
+    type: 'bar',
+    data: {
+        labels: ['2018', '2019', '2020', '2021', '2022'],
+        datasets: [{
+            label: 'Vacinados',
+            data: [45, 15, 50, 68, 79],
             borderWidth: 1,
             borderColor: '#0A4D68',
             backgroundColor: '#0A4D68'
@@ -447,4 +986,33 @@ function abrirNotificacao(){
     var notificacao = document.getElementsByClassName('notificacoes')[0];
 
     notificacao.style.display = 'flex';
+}
+
+function alterarDoenca(){
+    var doenca = doencaSelect.value;
+    var coqueluche = document.getElementsByClassName('coqueluche');
+    var meningite = document.getElementsByClassName('meningite');
+    var poliomielite = document.getElementsByClassName('poliomielite');
+    console.log(doenca);
+
+if(doenca == 'Coqueluche'){
+    for(var i = 0; i < coqueluche.length; i++){
+        coqueluche[i].style.display = 'flex';
+        meningite[i].style.display = 'none';
+        poliomielite[i].style.display = 'none';
+    }
+}else if(doenca == 'Meningite'){
+    for(var j = 0; j < meningite.length; j++){
+        coqueluche[j].style.display = 'none';
+        meningite[j].style.display = 'flex';
+        poliomielite[j].style.display = 'none';
+    }
+}else{
+    for(var k = 0; k < poliomielite.length; k++){
+        coqueluche[k].style.display = 'none';
+        meningite[k].style.display = 'none';
+        poliomielite[k].style.display = 'flex';
+    }
+}
+
 }
