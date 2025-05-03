a53908423469986296f713ff0eb00cb7b00758dd
fabiamdamaceno <fabiam.silva@sptech.school>
2025-05-03 20:47:42 -0300

    Alterado scripts

--- Script-v2.sh
+++ Script-v2.sh
@@ -1,49 +1,54 @@
 #!/bin/bash
 
-java -version
-
-if [ $? = 0 ];
-	then
-		echo "java instalado"
-
-	else 
-		echo "java não instalado"
-		echo "gostaria de instalar o java [s/n]"
-
-		read get
-
-	if [ \"$get\" == \"s\" ];
-		then
-		sudo apt install openjdk-17-jre -y
-
-	fi
-fi
-
-
 echo "limpando containers parados"
 sudo docker container prune -f
 
-echo "excluindo imagens"
+echo "excluindo imagens sem utilizar"
 sudo docker image prune -a -f
 
 echo "reiniciando docker network"
 sudo docker network rm network-immuno
 
+# Docker network para os containers se comunicarem
 echo "iniciando docker network"
 sudo docker network create network-immuno
 
-echo "buildando docker BD"
-sudo docker build -f ./projeto-pi-2o-semestre/script_banco/Dockerfile-Sql -t imagem-bancoimmuno ./projeto-pi-2o-semestre/script_banco
+# Verificando se o Banco está rodando
+sudo docker ps --filter "name=ContainerBanco" --filter "status=running" | grep "ContainerBanco" > /dev/null
 
-echo "rodando imagem docker"
-sudo docker run -d --name ContainerBanco --network network-immuno -p 3306:3306 imagem-bancoimmuno
+if [ $? = 0 ];
+	then
+		echo "container BD rodando"
 
-echo "definindo ipv4 da instancia"
-echo "APP_HOST=$(curl -s ifconfig.me)"
-echo "APP_HOST=$(curl -s ifconfig.me)" >> ./projeto-pi-2o-semestre/script_site/config.txt
+	else
+		echo "container BD não rodando"
 
-echo "buildando site"
-sudo docker build -f ./projeto-pi-2o-semestre/script_site/Dockerfile-Site -t imagem-siteimmuno ./projeto-pi-2o-semestre/script_site
+		echo "buildando docker BD"
+		sudo docker build -f ./projeto-pi-2o-semestre/script_banco/Dockerfile-Sql -t imagem-bancoimmuno ./projeto-pi-2o-semestre/script_banco
 
-echo "rodando imagem docker site"
-sudo docker run -d --name ContainerSite --network network-immuno -p 80:80 imagem-siteimmuno
+		echo "rodando imagem docker"
+		sudo docker run -d --name ContainerBanco --network network-immuno -p 3306:3306 imagem-bancoimmuno
+fi
+
+# Verificando se o Site está rodando
+sudo docker ps --filter "name=ContainerSite" --filter "status=running" | grep "ContainerSite" > /dev/null
+
+if [ $? = 0 ];
+	then
+		echo "container site rodando"
+
+	else
+		echo "container site não rodando"
+
+		echo "definindo ipv4 da instancia"
+		echo "APP_HOST=$(curl -s ifconfig.me)"
+		echo "APP_HOST=$(curl -s ifconfig.me)" >> ./projeto-pi-2o-semestre/script_site/config.txt
+
+		echo "buildando site"
+		sudo docker build -f ./projeto-pi-2o-semestre/script_site/Dockerfile-Site -t imagem-siteimmuno ./projeto-pi-2o-semestre/script_site
+
+		echo "rodando imagem docker site"
+		sudo docker run -d --name ContainerSite --network network-immuno -p 80:80 imagem-siteimmuno
+fi
+
+echo "processo finalizado"
