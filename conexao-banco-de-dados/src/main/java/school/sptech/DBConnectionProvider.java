61e046670525b2056f686e4e20e143a7b8b91b61
fabiamdamaceno <fabiam.silva@sptech.school>
2025-05-19 17:56:01 -0300

    Adicionado sout para debug

--- conexao-banco-de-dados/src/main/java/school/sptech/DBConnectionProvider.java
+++ conexao-banco-de-dados/src/main/java/school/sptech/DBConnectionProvider.java
@@ -7,9 +7,13 @@ public class DBConnectionProvider {
     public JdbcTemplate getJdbcTemplate() {
         BasicDataSource basicDataSource = new BasicDataSource();
         basicDataSource.setUrl(System.getenv("JAVA_URL"));
+        System.out.println(System.getenv("JAVA_URL"));
         basicDataSource.setUsername(System.getenv("JAVA_USERNAME"));
+        System.out.println(System.getenv("JAVA_USERNAME"));
         basicDataSource.setPassword(System.getenv("JAVA_PASSWORD"));
+        System.out.println(System.getenv("JAVA_PASSWORD"));
         basicDataSource.setDriverClassName(System.getenv("JAVA_CLASS_NAME"));
+        System.out.println(System.getenv("JAVA_CLASS_NAME"));
         return new JdbcTemplate(basicDataSource);
     }
 
