101840f539220c36094a8703d18cd37234d8a36d
Mateus <mateus.pereira@sptech.school>
2025-05-30 20:18:15 -0300

    lint

--- site/public/js/dashboard.js
+++ site/public/js/dashboard.js
@@ -1107,7 +1107,7 @@ fetch("/medidas/alterarDoenca", {
                     meningite[j].style.display = 'flex';
                     poliomielite[j].style.display = 'none';
                 }
-            }else if(json.nomeDoenca == 'Poliomielite'){
+            }else {
                 for(var k = 0; k < poliomielite.length; k++){
                     coqueluche[k].style.display = 'none';
                     meningite[k].style.display = 'none';
