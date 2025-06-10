497ae423f1bbce3f2576b87b400cccfc261edffa
Cynthia Angi <cynthia.ffa@gmail.com>
2025-06-09 22:14:06 -0300

    ajuste alterar doenca

--- site/public/js/dash-medico.js
+++ site/public/js/dash-medico.js
@@ -1109,7 +1109,7 @@ fetch("/medidas/alterarDoenca", {
                 }
             }
 
-        montarGraficoCidade(codigoCidade, json[0].idDoenca);
+        montarGraficoCidade(codigoCidade, json.idDoenca);
 
         });
 
